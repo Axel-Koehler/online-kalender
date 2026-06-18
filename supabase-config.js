0b1796b64d08c6cf133dd431e4429365a3682687
Axel-Koehler <axel.brueckner1401@gmail.com>
2026-06-18 12:28:57 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,7 @@
-url: "https://rglpcsnkctqukbvkwaeo.supabase.co",
-loginUsers: {
-  Axel: "axel.brueckner1401@gmail.com"
-}
+window.KALENDER_SUPABASE_CONFIG = {
+  url: "https://rglpcsnkctqukbvkwaeo.supabase.co",
+  anonKey: "sb_publishable_a6FcFWEoXqRJOYw7by2Jfg_LS5EjA8K",
+  loginUsers: {
+    Axel: "axel.brueckner1401@gmail.com"
+  }
+};
